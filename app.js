f06a882a06ec2222305976f1dd2e14db7f20aec2
Jeremy Worden <jeremy.worden@gmail.com>
2020-06-01 14:06:58 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const ngrok = require("ngrok");
+var indexRouter = require("./routes/index");
+var zoomRouter = require("./routes/zoom");
+var envMode = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+
+if (envMode === "development") {
+  // In development mode...let's start NGROK status page and open status page
+  (async function () {
+    await ngrok.connect({
+      proto: "http", // http|tcp|tls, defaults to http
+      addr: (result = process.env.PORT ? process.env.PORT : 3000), // port
+      subdomain: process.env.NGROK_SUBDOMAIN, // reserved tunnel name
+      authtoken: process.env.NGROK_AUTH_TOKEN, // your authtoken from ngrok.com
+      region: "us", // one of ngrok regions (us, eu, au, ap), defaults to us
+      onStatusChange: (status) => {}, // 'closed' - connection is lost, 'connected' - reconnected
+      onLogEvent: (data) => {}, // returns stdout messages from ngrok process
+    });
+  })();
+
+  // Disconnect Ngrok
+  process.once("SIGHUP", function () {
+    (async function () {
+      try {
+        await ngrok.kill();
+      } catch (error) {
+        console.log(error.message);
+      }
+    })();
+  });
+}
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/api/zoom", zoomRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ js/ciscoSoap.js
@@ -0,0 +1,179 @@
+"use strict";
+const axios = require('axios');
+var soap = require("strong-soap").soap;
+var request = require("request");
+var XMLHandler = soap.XMLHandler;
+var xmlHandler = new XMLHandler();
+var path = require("path");
+var specialRequest = request.defaults({
+  strictSSL: false,
+});
+
+var username = process.env.AXL_USERNAME;
+var password = process.env.AXL_PASSWORD;
+var auth = "Basic " + Buffer.from(username + ":" + password).toString("base64");
+
+var url = path.join(
+  __dirname,
+  "/axlsqltoolkit/schema/" + process.env.AXL_VERSION + "/AXLAPI.wsdl"
+);
+
+module.exports = {
+  getUserId: function (mailId) {
+    var options = {
+      endpoint: "https://" + process.env.AXL_IP + ":8443/axl/",
+      request: specialRequest,
+    };
+    var requestArgs = {
+      sql: "select userid from enduser where mailid='" + mailId + "'",
+    };
+    return new Promise(function (resolve, reject) {
+      soap.createClient(url, options, function (err, client) {
+        client.addHttpHeader("Authorization", auth);
+        var method = client.executeSQLQuery;
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+          if (err){
+            reject(err)
+          }
+          if(result.return){
+            resolve(result.return.row[0].userid);
+          }else{
+            resolve()
+          }
+        });
+      });
+    });
+  },
+  getDevice: function (userId) {
+    var options = {
+      endpoint: "https://" + process.env.AXL_IP + ":8443/axl/",
+      request: specialRequest,
+    };
+    var requestArgs = {
+      userid: userId,
+    };
+    return new Promise(function (resolve, reject) {
+      soap.createClient(url, options, function (err, client) {
+        client.addHttpHeader("Authorization", auth);
+        var method = client.getUser;
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+          if (err){
+            reject(err)
+          }
+          if(result.return.user.associatedDevices){
+            resolve(result.return.user.associatedDevices.device);
+          }else{
+            resolve()
+          }
+        });
+      });
+    });
+  },
+  addAppUser: function (userId,password,device) {
+    var options = {
+      endpoint: "https://" + process.env.AXL_IP + ":8443/axl/",
+      request: specialRequest,
+    };
+    var requestArgs = {
+      "appUser": {
+        "userid": userId,
+        "password": password,
+        "associatedDevices" :{
+          "device": device
+        },
+        "associatedGroups":{
+          "userGroup":{
+            "name": "Standard CTI Allow Control of All Devices"
+          }
+        }
+      },
+    };
+    return new Promise(function (resolve, reject) {
+      soap.createClient(url, options, function (err, client) {
+        client.addHttpHeader("Authorization", auth);
+        var method = client.addAppUser;
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+          if (err){
+            reject(err)
+          }else{
+            resolve(result);
+          }
+        });
+      });
+    });
+  },
+  removeAppUser: function (userId) {
+    var options = {
+      endpoint: "https://" + process.env.AXL_IP + ":8443/axl/",
+      request: specialRequest,
+    };
+    var requestArgs = {
+      userid: userId,
+    };
+    return new Promise(function (resolve, reject) {
+      soap.createClient(url, options, function (err, client) {
+        client.addHttpHeader("Authorization", auth);
+        var method = client.removeAppUser;
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+          if (err){
+            reject(err)
+          }else{
+            resolve(result);
+          }
+        });
+      });
+    });
+  },
+  getIpAddress: function (device) {
+    var url = path.join(
+      __dirname,
+      "/axlsqltoolkit/schema/RISService70.wsdl"
+    );
+    
+    var options = {
+      endpoint: "https://" + process.env.AXL_IP + ":8443/realtimeservice2/services/RISService70",
+      request: specialRequest,
+    };
+    var requestArgs = {
+      StateInfo: '',
+      CmSelectionCriteria: {
+        MaxReturnedDevices: "10",
+        DeviceClass: "Phone",
+        Model: "255",
+        Status: "Any",
+        NodeName: "",
+        SelectBy: "Name",
+        SelectItems: { item: {}},
+        Protocol: "Any",
+        DownloadStatus: "Any",
+      },
+    };
+    requestArgs.CmSelectionCriteria.SelectItems.item.Item = device
+    return new Promise(function (resolve, reject) {
+      soap.createClient(url, options, function (err, client) {
+        client.addHttpHeader("Authorization", auth);
+        var method = client.selectCmDeviceExt;
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+          if (err){
+            reject(err)
+          }
+          var ret = xmlHandler.xmlToJson(null, envelope, null)
+          if (ret.Body.selectCmDeviceResponse.selectCmDeviceReturn.SelectCmDeviceResult.TotalDevicesFound > 0){
+            var jsonResult = ret.Body.selectCmDeviceResponse.selectCmDeviceReturn.SelectCmDeviceResult.CmNodes.item
+          
+            let filteredResults = jsonResult.filter(function (e) {
+              return e.CmDevices;
+            });
+            if (filteredResults[0].CmDevices.item.Status === 'Registered'){
+              resolve(filteredResults[0].CmDevices.item.IPAddress.item.IP)
+            }else{
+              resolve()
+            }
+          }else{
+            resolve()
+          }
+        });
+      });
+    });
+  },
+};
